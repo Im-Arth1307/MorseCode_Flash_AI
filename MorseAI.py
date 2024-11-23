85386e829b39e0a1baa338ecf3dfa3452dd92c41
Atharva Jakhetiya <jakhetiyaatharva@gmail.com>
2024-11-23 17:13:48 +0530

    second

--- MorseAI.py
+++ MorseAI.py
@@ -80,3 +80,62 @@ class FlashlightMorseReceiver:
     def __init__(self):
         self.model = self.build_model()
         self.camera = None  # Initialize the camera object(None for now)
+        self.isrecording = False
+        self.buffer_size = 100
+        
+    def __del__(self):
+        self.stop_recording()
+        if self.camera:
+            self.camera.release()
+            
+    def build_model(self):
+        model = Sequential([
+            Conv3D(32, kernel_size = (3 , 3, 3), activation = 'relu', input_shape = (None, 64, 64, 1)),
+            MaxPooling3D(pool_size = (1, 2, 2)),
+            Conv3D(64, kernel_size = (3, 3, 3), activation = 'relu'),
+            MaxPooling3D(pool_size = (1, 2, 2)),
+            TimeDistributed(Flatten()),
+            LSTM(128, return_sequences = True),
+            LSTM(64),
+            Dense(32, activation = 'relu'),
+            Dense(len(MORSE_CODE), activation = 'softmax')
+        ])
+        
+    def _record_and_buffer(self):
+        frame_buffer = []
+        last_frame_time = time.time()
+        desired_fps = 30
+        frame_interval = 1.0 / desired_fps
+        
+        
+        while self.is_recording:
+            current_time = time.time()
+            if current_time - last_frame_time < frame_interval:
+                continue
+            
+            try:
+                ret, frame = self.camera.read()
+                if not ret:
+                    print("Failed to capture frame")
+                    continue
+        
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                resized = cv2.resize(gray, (64, 64))
+                normalized = resized / 255.0
+                
+                frame_buffer.append(normalized)
+                
+                if len(frame_buffer) > self.buffer_size:
+                    frame_buffer = frame_buffer[-self.buffer_size:]
+                    
+                if len(frame_buffer) == self.buffer_size:
+                    prediction = self._process_buffer(frame_buffer)
+                    self._decode_prediction(prediction)
+                    
+                last_frame_time = current_time
+                
+            except Exception as e:
+                print(f"Error while recording and buffering: {str(e)}")
+                time.sleep(0.1)
+                
+        
